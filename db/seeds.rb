606c3848fbee94aac23bd1ad7542a51aa9a33d3a
Gabriele Benevenia <gabrib1985@protonmail.com>
2022-07-08 01:32:48 +0200

    first part ok till CRUD / no nested stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+100.times do
+ restaurant =  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.subscriber_number(length: 9),
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  puts "#{restaurant.id} --> Restaurant #{restaurant.name} is a #{restaurant.category}"
+end
+puts "YAY"
